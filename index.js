c8eea00fac02896c4fe1725434acc9748b90edb5
t-cheepeng <tcp97@hotmail.com>
2020-05-22 17:22:49 +0800

    print req

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ function buildModeMenu() {
     telegrafBot.telegram.setWebhook(process.env.HEROKU_URL + process.env.BOT_TOKEN);
 
     app.get('/', (req, res) => {
+        console.log(req.message);
         res.send(req);
     });
 
